703778cfe6f6de9db91b94865107818399d74941
Fullooh <angstdemon1@aol.com>
2025-02-03 22:49:54 -0500

    Update build files for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,19 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    root: './',
     base: '/',
     server: {
         open: true
     },
     build: {
-        outDir: 'dist',
-        assetsDir: 'assets',
-        emptyOutDir: true,
+        outDir: '.',
+        emptyOutDir: false,
         cssCodeSplit: false,
         rollupOptions: {
             output: {
                 manualChunks: undefined,
+                entryFileNames: 'assets/[name]-[hash].js',
+                chunkFileNames: 'assets/[name]-[hash].js',
                 assetFileNames: (assetInfo) => {
                     if (assetInfo.name === 'index.css') return 'assets/style-[hash].css';
                     return 'assets/[name]-[hash][extname]';
