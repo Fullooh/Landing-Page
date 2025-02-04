1ac321538c60976c9889ba9fdbbdf6b9a26081a0
Fullooh <angstdemon1@aol.com>
2025-02-03 22:47:35 -0500

    Update build files

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,23 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
     root: './',
-    base: './',
+    base: '/',
     server: {
         open: true
+    },
+    build: {
+        outDir: 'dist',
+        assetsDir: 'assets',
+        emptyOutDir: true,
+        cssCodeSplit: false,
+        rollupOptions: {
+            output: {
+                manualChunks: undefined,
+                assetFileNames: (assetInfo) => {
+                    if (assetInfo.name === 'index.css') return 'assets/style-[hash].css';
+                    return 'assets/[name]-[hash][extname]';
+                },
+            },
+        },
     }
 });
